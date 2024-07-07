e6ad2b6d730c4edb85ff892bfb273a715eac4384
grzegorzkiselev <egorkakiselev@gmail.com>
2024-07-07 21:54:47 +0300

    chore: delete comments

--- do-request-with-retry.test.ts
+++ do-request-with-retry.test.ts
@@ -3,7 +3,7 @@ import { doRequestWithRetry } from "./do-request-with-retry";
 import { getServiceAxios } from "./service-axios";
 import { server } from "./mocks/node";
 import { http, HttpResponse } from "msw";
-import axios, { AxiosHeaders, AxiosResponse } from "axios";
+import { AxiosHeaders, AxiosResponse } from "axios";
 
 describe("Tecты попыток", () => {
   it("При ошибке выполняется нужное количество раз", async () => {
@@ -26,42 +26,13 @@ describe("Tecты попыток", () => {
     expect(mockFuncWithError.mock.calls.length).toBe(retryCount + 1);
   });
 
-  // it("Успешно выполняется после ошибок", async () => {
-  //   server.use(
-  //     http.get("https://jsonplaceholder.typicode.com/todos", () => {
-  //       return new HttpResponse(null, { status: 500 });
-  //     },
-  //     { once: true },
-  //     ),
-  //   );
-
-  //   const mockFuncWithError = jest.fn(() => getServiceAxios().get("https://jsonplaceholder.typicode.com/todos"));
-  //   const retryCount = 5;
-
-  //   await expect(
-  //     doRequestWithRetry(
-  //       mockFuncWithError,
-  //       { retryCount },
-  //     )).resolves.toHaveLength(4);
-
-  //   expect(mockFuncWithError.mock.calls.length).toBe(1 + 1);
-  // });
-
-  //class Response implements AxiosResponse {
-  //  data = "succeed";
-  //  status = 200;
-  //  statusText = "";
-  //  headers = {};
-  //  config = { headers: new AxiosHeaders() }
-  //}
-
   const Response: AxiosResponse = {
     data: "succeed",
     status: 200,
     statusText: "",
     headers: {},
     config: { headers: new AxiosHeaders() },
-  }
+  };
 
   it("Успешно выполняется после ошибок", async () => {
     const cb = () => Promise.resolve(Response);
