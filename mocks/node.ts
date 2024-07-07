56527e2c70b27ad07d127ad93e4def119f67cec6
grzegorzkiselev <egorkakiselev@gmail.com>
2024-07-07 16:31:43 +0300

    chore: remove comments

--- mocks/node.ts
+++ mocks/node.ts
@@ -1,18 +1,4 @@
-import { setupServer, SetupServerApi } from "msw/node";
+import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 
 export const server = setupServer(...handlers);
-
-// export const getMockNode = (() => {
-//   let server: null | SetupServerApi = null;
-
-//   return () => {
-//     if (server) {
-//       return server;
-//     }
-
-//     server = setupServer(...handlers);
-//     server.listen();
-//     return server;
-//   };
-// })();
