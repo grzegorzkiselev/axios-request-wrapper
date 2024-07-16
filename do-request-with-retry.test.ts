dbfb93c90afa79e28bfb5eeff73fe697bf65999e
grzegorzkiselev <egorkakiselev@gmail.com>
2024-07-16 22:10:03 +0300

    feat: move tests to the abstract responses

--- do-request-with-retry.test.ts
+++ do-request-with-retry.test.ts
@@ -1,20 +1,21 @@
 import { describe, expect, it, jest } from "@jest/globals";
-import { doRequestWithRetry } from "./do-request-with-retry";
-import { getServiceAxios } from "./service-axios";
-import { server } from "./mocks/node";
-import { http, HttpResponse } from "msw";
 import { AxiosHeaders, AxiosResponse } from "axios";
+import { doRequestWithRetry } from "./do-request-with-retry";
+
+const ResponseDummy: AxiosResponse = {
+  data: "succeed",
+  status: 200,
+  statusText: "",
+  headers: {},
+  config: { headers: new AxiosHeaders() },
+};
 
 describe("Tecты попыток", () => {
   it("При ошибке выполняется нужное количество раз", async () => {
-    server.use(
-      http.get("https://jsonplaceholder.typicode.com/todos", () => {
-        return new HttpResponse(null, { status: 500 });
-      },
-      ),
+    const mockFuncWithError = jest.fn(
+      () => Promise.reject(new Error()),
     );
 
-    const mockFuncWithError = jest.fn(() => getServiceAxios().get("https://jsonplaceholder.typicode.com/todos"));
     const retryCount = 5;
 
     await expect(
@@ -26,41 +27,32 @@ describe("Tecты попыток", () => {
     expect(mockFuncWithError.mock.calls.length).toBe(retryCount + 1);
   });
 
-  it("Успешно выполняется после ошибок", async () => {
-    const ResponseDummy: AxiosResponse = {
-      data: "succeed",
-      status: 200,
-      statusText: "",
-      headers: {},
-      config: { headers: new AxiosHeaders() },
-    };
-
+  it("Успешно выполняется после двух ошибок", async () => {
     const mockFuncWithError = jest.fn(
       () => Promise.resolve(ResponseDummy),
     );
 
     mockFuncWithError
-      .mockReturnValueOnce(Promise.reject(null));
+      .mockReturnValueOnce(Promise.reject(null).catch(e => e))
+      .mockReturnValueOnce(Promise.reject(null).catch(e => e));
 
+    const errorCount = 2;
     const retryCount = 5;
 
     await expect(
       doRequestWithRetry(
         mockFuncWithError,
         { retryCount },
-      )).resolves.toBe("succeed");
+      )
+    ).resolves.toBe("succeed");
 
-    expect(mockFuncWithError.mock.calls.length).toBe(1 + 1);
+    expect(mockFuncWithError.mock.calls.length).toBe(errorCount + 1);
   });
 
   it("Выполняется один раз, если количество попыток не задано", async () => {
-    server.use(
-      http.get("https://jsonplaceholder.typicode.com/todos", () => {
-        return new HttpResponse(null, { status: 500 });
-      }),
+    const mockFuncWithError = jest.fn(
+      () => Promise.reject(new Error()),
     );
-
-    const mockFuncWithError = jest.fn(() => getServiceAxios().get("https://jsonplaceholder.typicode.com/todos"));
 
     await expect(doRequestWithRetry(
       mockFuncWithError,
@@ -70,7 +62,9 @@ describe("Tecты попыток", () => {
   });
 
   it("Выполняется один раз, если ошибки не произошло", async () => {
-    const mockFuncWithoutError = jest.fn(() => getServiceAxios().get("https://jsonplaceholder.typicode.com/todos"));
+    const mockFuncWithoutError = jest.fn(
+      () => Promise.resolve(ResponseDummy),
+    );
 
     await expect(doRequestWithRetry(
       mockFuncWithoutError,
